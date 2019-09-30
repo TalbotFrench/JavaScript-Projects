6726354ade4ab33d02ab8875971f2529e7ef94da
Talbot French <talbot.french@gmail.com>
2019-09-30 15:00:16 -0600

    JS practice
    new javascript functions

---
+++ Basic-JavaScript-Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,6 @@
+var Family = "Quidelumine", Father = "Talbot", Mother = "Unknown", Child = " Valaerie Rode" ;
+window.alert(Family)
+var Father = Father.fontcolor("red")
+var Mother = Mother.fontcolor("blue")
+var Child = Child.fontcolor("purple")
+document.write(3+3)
---
+++ Basic-JavaScript-Projects/JS/Hello.js
@@ -0,0 +1,4 @@
+var Family = "Quidelumine", Father = "Talbot", Mother = "Unknown", Child = "Rode" ;
+window.alert(Family)
+document.write("\", for the Brethren"
++ " or for yourself?")
