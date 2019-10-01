db4c11f2e4e2856d035cf192a1680e4ea3afdbba
Talbot French <talbot.french@gmail.com>
2019-10-01 11:29:45 -0600

    Broken JavaScript
    my java script isnt working but hopefully I can get it too soon.

--- Basic-JavaScript-Projects/JS/Basic_JavaScript_1.js
+++ Basic-JavaScript-Projects/JS/Basic_JavaScript_1.js
@@ -1,6 +1,11 @@
 var Family = "Quidelumine", Father = "Talbot", Mother = "Unknown", Child = " Valaerie Rode" ;
-window.alert(Family)
 var Father = Father.fontcolor("red")
 var Mother = Mother.fontcolor("blue")
 var Child = Child.fontcolor("purple")
-document.write(3+3)
+
+
+function myFirstFunction() {
+    var str = "this text is green!";
+    var result = str.fontcolor("green");
+    document.getElementById("Green_Text").innerHTML = result;
+}
---
+++ Basic-JavaScript-Projects/JS/Basic_JavaScript_2.js
@@ -0,0 +1,16 @@
+var white = "yin"
+var black = "yang"
+var white = fontcolor("pink")
+var black = fontcolor("black")
+
+function p2(){
+    var String = "new thing"
+    var result = String.white();
+    document.getElementById("p1").innerHtml = result;
+}
+
+function myP(){
+    var sentence = "I am mad this";
+    sentence += " wont work and it makes me salty!";
+    document.getElementById("concatenate").innerHtml = sentence;
+}
