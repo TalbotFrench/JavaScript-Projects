febd921a21df4542db05820bc8c448d9b6800ba6
Talbot French <talbot.french@gmail.com>
2019-10-02 11:18:24 -0600

    update JavaScript
    practice JavaScript Functions

---
+++ Basic-JavaScript-Projects/JS/Basic_JavaScript_5.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    A = "Magnus";
+    B = "Magnus";
+    C = "Talbot";
+document.getElementById("Test").innerHTML = !(5 > 10) + " " + !(6 < 7); 
+console.log(2 >= 3);
+}
+
+
